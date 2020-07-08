42ed2e9021ddd7917249e4f4c471b0a30c277741
Marzia Fatarella <marziafatarella@gmail.com>
2020-07-08 09:22:28 +0200

    tabella

--- resources/views/clothes.blade.php
+++ resources/views/clothes.blade.php
@@ -4,11 +4,36 @@
 
 //definisco la section content
 @section('content')
-    @foreach ($vestiti as $vestito)
-        <p>{{ $vestito->name }}</p>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Tutti i vestiti</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>taglia</th>
+                            <th>colore</th>
+                            <th>nome</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($vestiti as $vestito)
+                            <tr>
+                                <td>{{ $vestito->id }}</td>
+                                <td>{{ $vestito->size }}</td>
+                                <td>{{ $vestito->color }}</td>
+                                <td>{{ $vestito->size }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
+//creo la struttura della tabella (anche con bootstrap volendo) e con un for each compilo tutti i campi
 
-\\definisco la section per il page title
+//definisco la section per il page title
 @section('page-title','Index Vestiti')
 
